774233f68e235a0aeb694fc1ec00fc3e58114126
nafiz <naafizrahman1@gmail.com>
2022-02-14 21:12:19 +0600

    Desktop Version Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 //Here is the script for the website
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
