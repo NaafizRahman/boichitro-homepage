63b29c704323cb9c0953305210ff1cc4c8114d0f
nafiz <naafizrahman1@gmail.com>
2022-02-14 22:42:54 +0600

    Mobile Version Update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-//Here is the script for the website
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+function toggleMobileMenu(menu) {
+  menu.classList.toggle("open");
 }
